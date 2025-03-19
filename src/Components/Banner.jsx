e83ed140dd4bfc737e8dacf70c9cfe205289ed08
cristian ballarino <cristianballarino1@gmail.com>
2025-03-19 03:07:59 -0300

    avance

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,14 @@
+import './banner.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>Bikes Bmx Shop</h1>
+            <p>La mejor tienda de Bmx Online</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
